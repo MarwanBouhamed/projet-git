6ae3c1b48e37337faaad4e9e169b05db9d6d29c5
Marwan bouhamid <marwanbouhamid123@gmail.com>
2025-09-17 13:19:19 +0100

    hello

--- script.js
+++ script.js
@@ -7,14 +7,14 @@
 
 
             //Chapitre 8
-//fonction 1
+//fonction 1 name
 function saluer(nom) {
     console.log("Bonjeur " + nom + " !");
 }
 console.log(saluer("Marwan"));
 
 
-//fonction 2
+//fonction 2 calcule
 
 function opper(a,b,c,d){
     return ((a + b) - c ) * d;
@@ -22,3 +22,15 @@ function opper(a,b,c,d){
 
 let oppertion = opper(60 , 40 , 20 , 2);
 console.log("la solution est:", oppertion);
+
+              //Chapitre 9
+
+let age = 20;
+
+if (age < 18) {
+      console.log("Vous êtes mineur.");
+} else if (age === 18) {
+      console.log("Vous venez d’atteindre la majorité !");
+} else {
+      console.log("Vous êtes majeur.");
+}
