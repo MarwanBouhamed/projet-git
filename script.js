8aad2333927a9ccf9136f4bc9ad835828d8860ac
Marwan bouhamid <marwanbouhamid123@gmail.com>
2025-09-17 12:42:51 +0100

    good

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-let FirstName = Marwan; let LastName = Bouhamid; console.log(my name is , FirstName , LastName);
+
+
+           //Chapitre 7
+// let FirstName = Marwan;
+// let LastName = Bouhamid;
+// console.log("my name is" , FirstName , LastName);
+
+
+            //Chapitre 8
+//fonction 1
+function saluer(nom) {
+    console.log("Bonjeur " + nom + " !");
+}
+console.log(saluer("Marwan"));
+
+
+//fonction 2
+
+function opper(a,b,c,d){
+    return ((a + b) - c ) * d;
+}
+
+let oppertion = opper(60 , 40 , 20 , 2);
+console.log("la solution est:", oppertion);
