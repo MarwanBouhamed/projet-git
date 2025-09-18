98c02ac97e2907831aac267aaa19a9d26c5a2c80
Marwan bouhamid <marwanbouhamid123@gmail.com>
2025-09-18 09:13:48 +0100

    added chapitre 10

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 
-
            //Chapitre 7
 // let FirstName = Marwan;
 // let LastName = Bouhamid;
-// console.log("my name is" , FirstName , LastName);
-
-
+// console.log("my name is" + FirstName + LastName)
             //Chapitre 8
 //fonction 1 name
 function saluer(nom) {
     console.log("Bonjeur " + nom + " !");
 }
-console.log(saluer("Marwan"));
-
-
-//fonction 2 calcule
+console.log(saluer("Marwan"))
+//fonction 2 calcul
 
 function opper(a,b,c,d){
     return ((a + b) - c ) * d;
 }
 
 let oppertion = opper(60 , 40 , 20 , 2);
-console.log("la solution est:", oppertion);
-
-              //Chapitre 9
-
-let age = 20;
-
+console.log("la solution est:", oppertion)
+              //Chapitre 
+let age = 20
 if (age < 18) {
       console.log("Vous êtes mineur.");
 } else if (age === 18) {
@@ -34,3 +26,13 @@ if (age < 18) {
 } else {
       console.log("Vous êtes majeur.");
 }
+          //Chapitre 10
+
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 5 && i === 8) continue; // sauter le 5
+  console.log("Nombre : " + i);
+}
+              
+
+
